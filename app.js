a304dc6206497fe6f38c20bcc0d37df0bb073350
salim <salim.salih1991@gmail.com>
2022-09-03 19:39:21 +0430

    git add

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express')
+var app =express();
+var path =require('path');
+
+var expresslayout = require('express-ejs-layouts');
+var indexRouter = require('./routes/index');
+var registrationRouter = require('./routes/registration');
+
+
+app.set('layout','./mainlayouts/layout');
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,'public')));
+app.use(expresslayout);
+app.set(express.urlencoded({extended:false}));
+app.set(express.join);
+app.use(expresslayout)
+app.use('/',indexRouter)
+app.use('/registration',registrationRouter);
+app.listen(3000);
+module.express= app;
+
